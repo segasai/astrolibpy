4c27ab030ee83e9cdb5fb8d052f99d345ec5ceaa
koposov <koposov@8e70b0ce-f37c-11dd-afc9-ff9d66c6243e>
2009-02-09 13:35:22 +0000

    large fixes in readcol
    replace scipy.io.read_array by numpy.loadtxt since the former is deprecated
    
    git-svn-id: https://astrolibpy.googlecode.com/svn/trunk@6 8e70b0ce-f37c-11dd-afc9-ff9d66c6243e

--- readcol.py
+++ readcol.py
@@ -1,16 +1,59 @@
 import scipy.io
+import numpy 
 
-def readcol(filename, separator=' ', format=None):
+def readcol(filename, delimiter=' ', format=None, skiprows=0):
 	if format==None:
 		print 'x'
-		res=scipy.io.read_array(filename, separator=separator)
-		ncols = res.shape[1]
+		res=numpy.loadtxt(filename, delimiter=delimiter)
+		ncols = res.shape[0]
 		nrows = res.shape[1]
-		buf="("
+		stor=[]
 		for i in range(ncols):
-			exec("a%d = res[:,i]"%i)
-			buf=buf+"a%d,"%i
-		buf=buf[:-1]+")"
-		exec("result=%s"%buf)
-		return result
+			stor.append(res[:,i])
+		return tuple(stor)
+	else:
+		types=[]
+		i=0
+		formats=format.split(',')
+		convs={}
+		
+		retnull = lambda s: numpy.float(s or 0)
+		for i, a in enumerate(formats):
+			if a=='I':
+				curtype=numpy.int32
+				convs[i]=retnull
+			elif a=='F':
+				curtype=numpy.float32
+				convs[i]=retnull
+			elif a=='D':
+				curtype=numpy.float64
+				convs[i]=retnull
+			elif a=='S':
+				curtype="S100"#numpy.str
+			types.append(("a%d"%i,curtype))
+		print types
+		
+		rec=numpy.loadtxt(file(filename),dtype=types, delimiter=delimiter, 
+						skiprows=skiprows,converters=convs)
+		print rec[0]
+		ncols=len(rec[0])
+		nrows=len(rec)
+
+		buf="("
+		stor=[]
+		for a in formats:
+			if a=='I':
+				tmp=numpy.zeros(nrows,dtype=numpy.int32)
+			elif a=='F':
+				tmp=numpy.zeros(nrows,dtype=numpy.float32)
+			elif a=='D':
+				tmp=numpy.zeros(nrows,dtype=numpy.float64)
+			elif a=='S':
+				tmp=numpy.zeros(nrows,dtype=numpy.str)
+			stor.append(tmp)
+
+		for i in range(ncols):
+			for j in range(nrows):
+				stor[i][j]=rec[j][i]
+		return tuple(stor)
 #	print 'z',format,format==None
