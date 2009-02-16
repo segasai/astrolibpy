f84358d129076f46828e834e921d67c9dba76b77
koposov <koposov@8e70b0ce-f37c-11dd-afc9-ff9d66c6243e>
2009-02-16 21:02:05 +0000

    Add the code by Dima Semenov for doing contour plotting
    Also, I updated the plot() routine to handle the cases when only one argument
    is given
    
    git-svn-id: https://astrolibpy.googlecode.com/svn/trunk@22 8e70b0ce-f37c-11dd-afc9-ff9d66c6243e

--- idlplot.py
+++ idlplot.py
@@ -54,7 +54,7 @@ def plothist(x,bin=None, xrange=None, yrange=None, min=None, max=None,
 	tmp(loc1,hh1,ps=0,color=color,xrange=xrange,yrange=yrange,
 		xlog=xlog,ylog=ylog, **kw)
     
-def plot (x,y, xrange=None, yrange=None, ps=0, thick=1, xtitle="", ytitle="",
+def plot (arg1, arg2=None, xrange=None, yrange=None, ps=0, thick=1, xtitle="", ytitle="",
 		color='black', noerase=False, overplot=False,position=None, ylog=False,
 		xlog=False, xr=None, yr=None):
 	""" Plot your data in an IDL-way
@@ -62,6 +62,12 @@ def plot (x,y, xrange=None, yrange=None, ps=0, thick=1, xtitle="", ytitle="",
 		plot(x,y,xrange=[0,39],yrange=[-1,10],ps=4,xtitle="X",\
 			color='black',position=[0.1,0.1,0.9,0.9], xlog=True)
 	"""
+	if arg2==None:
+		y=arg1
+		x=numpy.arange(len(y))
+	else:
+		x=arg1
+		y=arg2
 	if not noerase:
 		plt.gcf().clf()	
 	if position!=None:
@@ -122,3 +128,130 @@ def ploterror (x,y, err, color='black', ps=0, ecolor='black', overplot=False,
 
 def tvaxis (image, xmin, xmax,ymin,ymax):
 	pass
+
+def contour (z, x=None, y=None, xrange=None, yrange=None, zrange=None, xr=None, yr=None, zr=None, 
+		title="", xtitle="", ytitle="", position=None, xlog=False, ylog=False, zlog=False, 
+		xticklabel = "%1.1f", yticklabel = "%1.1f", zticklabel = "%1.1f", levels=None, 
+		nlevels=256, c_color="black", cm ="jet", c_line="solid", c_levels=None, c_charsize=12.0, 
+		c_thick=1, thick=1, font="monospace", weight="normal", charsize=14.0, bar=True, fill=True, 
+		overplot=False, noerase=False):
+
+# Adjust some built-in parameters to make plots nicer:		
+#	matplotlib.rcParams['figure.facecolor']='white'
+#	matplotlib.rcParams['text.usetex']=True
+	matplotlib.rcParams['font.family']=font
+	matplotlib.rcParams['font.size']=charsize
+	matplotlib.rcParams['font.weight']=weight
+	matplotlib.rcParams['axes.labelsize']=charsize	
+	matplotlib.rcParams['xtick.labelsize']=charsize	
+	matplotlib.rcParams['ytick.labelsize']=charsize			
+	matplotlib.rcParams['lines.markeredgewidth']=thick-0.5	
+	matplotlib.rcParams['lines.linewidth']=thick
+	matplotlib.rcParams['lines.antialiased']=True	
+	matplotlib.rcParams['xtick.minor.size']=7
+	matplotlib.rcParams['ytick.minor.size']=7		
+	matplotlib.rcParams['xtick.major.size']=12
+	matplotlib.rcParams['ytick.major.size']=12		
+#	matplotlib.rcParams['ps.papersize']=papersize
+
+
+# Initialize x and y if these are not provided:
+	if x==None or y==None:
+		x, y = meshgrid(arange(z.shape[0]), arange(z.shape[1]))
+		
+# Define position of this plot:
+	if not noerase:
+		plt.gcf().clf()	
+		if position!=None:
+			mypos=position[:]
+			mypos[2]=position[2]-position[0]
+			mypos[3]=position[3]-position[1]
+			plt.axes(mypos)
+
+# Rescaling of the axes:		
+	if xlog:
+	 	plt.gca().set_xscale('log')
+	if ylog:
+		plt.gca().set_yscale('log')
+	if zlog:
+		z = log10(z)
+
+# Setup axis ranges:			
+	if xr!=None:
+		xrange=xr
+	if yr!=None:
+		yrange=yr
+	if zr!=None:
+		zrange=zr
+
+	if xrange==None:
+		xrange=[x.min(),x.max()]
+	if yrange==None:
+		yrange=[y.min(),y.max()]
+	if zrange==None:
+		zrange=[z.min(),z.max()]		
+		
+# Setup levels for contour plot:		
+	if levels==None:
+		zmin = zrange[0]
+		zmax = zrange[1]
+		dz = zmax-zmin
+		levels = zmin + dz/nlevels*arange(nlevels+1)	
+
+# Setup frame thickness:
+	plt.gca().frame.set_linewidth(thick) 
+
+# Setup x-, y-titles and the main title:
+	plt.gca().set_xlabel(xtitle)
+	plt.gca().set_ylabel(ytitle)
+	plt.title(title)
+
+# Assume autoscale is off:	
+	plt.gca().set_autoscale_on(False)
+	
+# For a new plot, we have to initialize axes:	
+	if not overplot:
+		plt.gca().axis(xrange+yrange) 
+
+# Setup format of the major ticks:
+	xFormatter = ticker.FormatStrFormatter(xticklabel)
+	yFormatter = ticker.FormatStrFormatter(yticklabel)
+	plt.gca().xaxis.set_major_formatter(xFormatter)
+	plt.gca().yaxis.set_major_formatter(yFormatter)
+
+# Setup minor tickmarks:		
+	xminorLocator = MaxNLocator(nbins=90, steps=[1, 2, 5, 10])
+	yminorLocator = MaxNLocator(nbins=90, steps=[1, 2, 5, 10])	
+
+	plt.gca().xaxis.set_minor_locator(xminorLocator)
+	plt.gca().yaxis.set_minor_locator(yminorLocator)		
+		
+# Make a filled contour plot:		
+	if fill:
+		cset1=plt.gca().contourf(x, y, z, levels, cmap=plt.cm.get_cmap(cm, nlevels))
+
+# Add contour lines:
+	if c_levels == None:
+		c_levels = levels[0:len(levels):int(nlevels/12)]
+	cset2 = plt.gca().contour(x, y, z, c_levels, colors = c_color,linewidths=c_thick,hold='on')
+
+# Do not display dashed contours for negative values:
+	for c in cset2.collections:
+   		c.set_linestyle(c_line)
+
+# Add value labels on contour lines:
+	zFormatter = ticker.FormatStrFormatter(zticklabel)
+	cset3 = plt.gca().clabel(cset2, c_levels, inline=1, fmt=zticklabel, fontsize=c_charsize)
+                
+# Do we need a color bar?:                
+	if bar:
+		matplotlib.rcParams['ytick.labelsize']=c_charsize				
+		plt.colorbar(cset1, ticks=[min(levels), max(levels)], shrink = 0.87, aspect=15, 
+			format=zFormatter)
+		
+	if plt.isinteractive():
+		plt.draw()
+		
+# Restore default matplotlib parameters:
+	matplotlib.rcdefaults()
+
