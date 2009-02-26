104f6e87821f2e157e4e75247e9d2dc891c41709
koposov <koposov@8e70b0ce-f37c-11dd-afc9-ff9d66c6243e>
2009-02-26 04:43:10 +0000

    add the parameters option and properly treat the unicode strings
    git-svn-id: https://astrolibpy.googlecode.com/svn/trunk@38 8e70b0ce-f37c-11dd-afc9-ff9d66c6243e

--- sqlutil.py
+++ sqlutil.py
@@ -3,7 +3,7 @@ import sqlite3
 import types
 import numpy
 
-def get(query, db="wsdb", driver="pgdb"):
+def get(query, params=None, db="wsdb", driver="pgdb"):
 	if driver=='pgdb':
 		con = pgdb.connect(database=db)
 	elif driver=='sqlite3':
@@ -11,7 +11,10 @@ def get(query, db="wsdb", driver="pgdb"):
 	else: 
 		raise Exception("Unknown driver")
 	cur = con.cursor()
-	res = cur.execute(query)
+	if params==None:
+		res = cur.execute(query)
+	else:
+		res = cur.execute(query, params)
 	tups = cur.fetchall()
 	nrows = len(tups)
 	if nrows == 0:
@@ -20,8 +23,10 @@ def get(query, db="wsdb", driver="pgdb"):
 	ncols = len(line)
 	buf = []
 	for curtype in map(lambda s: type(s),line):
-		if curtype == types.StringType:
-			buf.append(range(nrows))
+		if curtype == types.StringType or curtype == types.UnicodeType:
+			#TODO I'm now using the strings which are 100 char long 
+			# probably it should be hardcoded that way.. 
+			buf.append(numpy.zeros(nrows, dtype='|S100'))
 		else:
 			buf.append( numpy.zeros(nrows, dtype=curtype))
 
