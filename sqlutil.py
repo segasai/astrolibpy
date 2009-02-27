d4d8fd0f3d2022cfeb5622a53cd868d4b83e474c
koposov <koposov@8e70b0ce-f37c-11dd-afc9-ff9d66c6243e>
2009-02-27 13:24:11 +0000

    Move the importing  inside the functio Add some docs
    git-svn-id: https://astrolibpy.googlecode.com/svn/trunk@43 8e70b0ce-f37c-11dd-afc9-ff9d66c6243e

--- sqlutil.py
+++ sqlutil.py
@@ -1,12 +1,20 @@
-import pgdb
-import sqlite3
 import types
 import numpy
 
 def get(query, params=None, db="wsdb", driver="pgdb"):
+	'''This program executes the sql query and returns 
+	the tuple of the numpy arrays.
+	Example:
+	a,b, c = sqlutil.get('select ra,dec,d25 from rc3')
+	You can also use the parameters in your query:
+	Example:
+	a,b = squlil.get('select ra,dec from rc3 where name=?',"NGC 3166")
+	'''
 	if driver=='pgdb':
+		import pgdb
 		con = pgdb.connect(database=db)
 	elif driver=='sqlite3':
+		import sqlite3
 		con = sqlite3.connect(db)
 	else: 
 		raise Exception("Unknown driver")
