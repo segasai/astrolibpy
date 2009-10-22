7783c08b506eedbd0f9e9725d915d72f312d72da
koposov <koposov@8e70b0ce-f37c-11dd-afc9-ff9d66c6243e>
2009-10-22 11:38:24 +0000

    fix the bug in the previous correction of sphdist
    git-svn-id: https://astrolibpy.googlecode.com/svn/trunk@124 8e70b0ce-f37c-11dd-afc9-ff9d66c6243e

--- astrolib/sphdist.py
+++ astrolib/sphdist.py
@@ -7,8 +7,8 @@ def sphdist (ra1, dec1, ra2, dec2):
 	dec1_r=numpy.deg2rad(dec1)
 	dec2_r=numpy.deg2rad(dec2)
 	return 2*numpy.rad2deg(numpy.arcsin(numpy.sqrt(
-		(numpy.sin((dec1r-dec2r)/2))**2+ 
-		numpy.cos(dec1r)*numpy.cos(dec2r)*
+		(numpy.sin((dec1_r-dec2_r)/2))**2+ 
+		numpy.cos(dec1_r)*numpy.cos(dec2_r)*
 		(numpy.sin((numpy.deg2rad(ra1-ra2))/2))**2
 		)))
 	
