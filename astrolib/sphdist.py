01cdabbb2dbed479acfa7b6b6940db32574b3f7c
koposov <koposov@8e70b0ce-f37c-11dd-afc9-ff9d66c6243e>
2009-10-22 11:34:17 +0000

    sphdist now should use much less memory: I removed the creation of two temporary variables
    git-svn-id: https://astrolibpy.googlecode.com/svn/trunk@122 8e70b0ce-f37c-11dd-afc9-ff9d66c6243e

--- astrolib/sphdist.py
+++ astrolib/sphdist.py
@@ -1,13 +1,14 @@
 import numpy
 
-def sphdist (ra1, dec1,ra2,dec2):
-	"""measures the spherical distance in degrees"""
-	ra1r=numpy.deg2rad(ra1)
-	ra2r=numpy.deg2rad(ra2)
-	dec1r=numpy.deg2rad(dec1)
-	dec2r=numpy.deg2rad(dec2)
+def sphdist (ra1, dec1, ra2, dec2):
+	"""measures the spherical distance in degrees
+		The input has to be in degrees too
+	"""
+	dec1_r=numpy.deg2rad(dec1)
+	dec2_r=numpy.deg2rad(dec2)
 	return 2*numpy.rad2deg(numpy.arcsin(numpy.sqrt(
 		(numpy.sin((dec1r-dec2r)/2))**2+ 
-		numpy.cos(dec1r)*numpy.cos(dec2r)*(numpy.sin((ra1r-ra2r)/2))**2
+		numpy.cos(dec1r)*numpy.cos(dec2r)*
+		(numpy.sin((numpy.deg2rad(ra1-ra2))/2))**2
 		)))
 	
