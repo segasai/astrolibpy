be638acef24744e49546f10963d54d0a1219ddcd
Sergey Koposov <math@fortune.mpia-hd.mpg.de>
2009-10-23 22:33:19 +0200

    make the new faster implementation ...

--- utils/sqlutil.py
+++ utils/sqlutil.py
@@ -1,5 +1,38 @@
 import types
 import numpy
+import time
+
+
+def fetchmany(curs, size=None, keep=False):
+	"""Fetch the next set of rows of a query result.
+
+	The number of rows to fetch per call is specified by the
+	size parameter. If it is not given, the cursor's arraysize
+	determines the number of rows to be fetched. If you set
+	the keep parameter to true, this is kept as new arraysize.
+
+	"""
+	_cast = {'bool': numpy.bool,
+		'int2': numpy.int16, 'int4': numpy.int32, 'serial': numpy.int32,
+		'int8': numpy.int64, 'oid': numpy.int64, 'oid8': numpy.int64,
+		'float4': numpy.float32, 'float8': numpy.float64,
+		'numeric': numpy.float128, 'varchar': numpy.str}
+
+
+	if size is None:
+		size = curs.arraysize
+	if keep:
+		curs.arraysize = size
+	try:
+		result = curs._src.fetch(size)
+	except Error, err:
+		raise pgdb.DatabaseError(str(err))
+	row_factory = curs.row_factory
+	typecast = curs._type_cache.typecast
+	coltypes = [desc[1] for desc in curs.description]
+	types=[_cast.get(a) for a in coltypes]
+	return result,types #[list(row) for row in result]
+
 
 def get(query, params=None, db="wsdb", driver="pgdb"):
 	'''This program executes the sql query and returns 
@@ -23,13 +56,31 @@ def get(query, params=None, db="wsdb", driver="pgdb"):
 		res = cur.execute(query)
 	else:
 		res = cur.execute(query, params)
-	tups = cur.fetchall()
+
+	tups,types = fetchmany(cur,-1, False)
+
 	cur.close()
 	con.commit()
 	con.close()
 	nrows = len(tups)
+#	print nrows
 	if nrows == 0:
 		return None
-	res = numpy.array(tups)
+	for curtype in [numpy.float64,numpy.float32,numpy.int16,numpy.int32,numpy.int64]:
+		if numpy.array([a ==curtype  for a in types]).all():
+			res= numpy.asfarray(tups,types[0])
+			return res
 
-	return res.transpose()
+	res = numpy.array(tups)
+	nrows,ncols=res.shape
+	result = []
+	for  i, cur_type in zip(range(ncols),types):
+		if cur_type == numpy.str:
+			result.append(numpy.fromiter(res[:,i],dtype='|S100'))
+			(result[-1])[result[-1]=='None']=''
+
+		else:
+			result.append(res[:,i].astype(cur_type))
+	
+	return result		
+	#return [res[:,a].astype(b) for a,b in zip(range(ncols),types)]
