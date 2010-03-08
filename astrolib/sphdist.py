da9e78892a500a327a2b4b8d12b9f154d2c9da71
koposov <koposov@8e70b0ce-f37c-11dd-afc9-ff9d66c6243e>
2010-03-08 19:59:31 +0000

    slightly clean-up the spdist function
    git-svn-id: https://astrolibpy.googlecode.com/svn/trunk@141 8e70b0ce-f37c-11dd-afc9-ff9d66c6243e

--- astrolib/sphdist.py
+++ astrolib/sphdist.py
@@ -1,14 +1,27 @@
-import numpy
+from numpy import deg2rad,rad2deg,sin,cos,sqrt,arcsin
 
 def sphdist (ra1, dec1, ra2, dec2):
 	"""measures the spherical distance in degrees
 		The input has to be in degrees too
 	"""
-	dec1_r=numpy.deg2rad(dec1)
-	dec2_r=numpy.deg2rad(dec2)
-	return 2*numpy.rad2deg(numpy.arcsin(numpy.sqrt(
-		(numpy.sin((dec1_r-dec2_r)/2))**2+ 
-		numpy.cos(dec1_r)*numpy.cos(dec2_r)*
-		(numpy.sin((numpy.deg2rad(ra1-ra2))/2))**2
-		)))
+	dec1_r = deg2rad(dec1)
+	dec2_r = deg2rad(dec2)
+	return 2 *\
+		rad2deg \
+			(
+			arcsin
+				(
+				sqrt
+					(
+						(
+							sin((dec1_r - dec2_r) / 2)
+						)**2
+						+  
+						cos(dec1_r) * cos(dec2_r) *
+						(
+							sin((deg2rad(ra1 - ra2)) / 2)
+						)**2
+					)
+				)
+			)
 	
