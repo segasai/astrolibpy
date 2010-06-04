0d0b80d63789ce338e9d51c68f04f83019303bef
koposov <koposov@8e70b0ce-f37c-11dd-afc9-ff9d66c6243e>
2010-06-04 19:17:38 +0000

    slight style cleanup
    git-svn-id: https://astrolibpy.googlecode.com/svn/trunk@162 8e70b0ce-f37c-11dd-afc9-ff9d66c6243e

--- astrolib/cv_coord.py
+++ astrolib/cv_coord.py
@@ -1,14 +1,17 @@
 import numpy
+
 def cv_coord(a,b,c,fr=None,to=None,degr=False):
 	if degr:
-		radec = 180./numpy.pi
+		degrad = numpy.deg2rad
+		raddeg = numpy.rad2deg
 	else:
-		radec= 1
+		degrad = lambda x: x
+		raddeg = lambda x: x
 	if fr=='sph':
-		cosa = numpy.cos(a/radec)
-		sina = numpy.sin(a/radec)
-		cosb = numpy.cos(b/radec)
-		sinb = numpy.sin(b/radec)
+		cosa = numpy.cos(degrad(a))
+		sina = numpy.sin(degrad(a))
+		cosb = numpy.cos(degrad(b))
+		sinb = numpy.sin(degrad(b))
 		x=c*cosa*cosb
 		y=c*sina*cosb
 		z=c*sinb
@@ -17,18 +20,18 @@ def cv_coord(a,b,c,fr=None,to=None,degr=False):
 		y=b
 		z=c
 	elif fr is None:
-		raise Exception('Please specify the input coordinate system')
+		raise Exception('You must specify the input coordinate system')
 	else:
 		raise Exception('Unknown input coordinate system')
 	if to=='rect':
 		return (x,y,z)
 	elif to=='sph':
-		ra = numpy.arctan2(y,x)*radec
-		dec = numpy.arctan2(z,numpy.sqrt(x**2+y**2))*radec
+		ra = raddeg(numpy.arctan2(y,x))
+		dec = raddeg(numpy.arctan2(z,numpy.sqrt(x**2+y**2)))
 		rad = numpy.sqrt(x**2+y**2+z**2)
 		return (ra,dec,rad)
 	elif to is None:
-		raise Exception('Please specify the output coordinate system')
+		raise Exception('You must specify the output coordinate system')
 	else:
 		raise Exception('Unknown output coordinate system')
 
