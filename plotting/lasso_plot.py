82288ae2e40f7d7694778174d3933d8d5dbeb40f
koposov <koposov@8e70b0ce-f37c-11dd-afc9-ff9d66c6243e>
2010-06-15 02:59:15 +0000

    clean up of the lasso plot
    git-svn-id: https://astrolibpy.googlecode.com/svn/trunk@170 8e70b0ce-f37c-11dd-afc9-ff9d66c6243e

--- plotting/lasso_plot.py
+++ plotting/lasso_plot.py
@@ -1,22 +1,19 @@
 from matplotlib.widgets import Lasso
 from matplotlib.nxutils import points_inside_poly
-from matplotlib.colors import colorConverter
-from matplotlib.collections import RegularPolyCollection
-
 from matplotlib.pyplot import gca
 from numpy import nonzero
 
 class lasso_plot:
-    # The class is designed to select the datapoints by drawing the region
-    # around it. 
-    # Example:
-    # plot(xs, ys, ps=3 ) # first plot the data
-    # las = lasso_plot.lasso_plot(xs,ys)
-    # Now click on the plot and do not release the mouse button till 
-    # you draw your region 
-    # After that the variable las.ind will contain the indices of those 
-    # points inside the region and las.verts will contain the vertices of the 
-    # polygon you've just drawn 
+    """ The class is designed to select the datapoints by drawing the region
+    around it. 
+    Example:
+    plot(xs, ys, ps=3 ) # first plot the data
+    las = lasso_plot.lasso_plot(xs,ys)
+    Now click on the plot and do not release the mouse button till 
+    you draw your region 
+    After that the variable las.ind will contain the indices of those 
+    points inside the region and las.verts will contain the vertices of the 
+    polygon you've just drawn """
     def __init__(self, xs, ys):
         self.axes = gca()
         self.canvas = self.axes.figure.canvas
@@ -24,12 +21,14 @@ class lasso_plot:
         fig = self.axes.figure
         self.cid = self.canvas.mpl_connect('button_press_event', self.onpress)
         self.ind = None
+        self.verts = None
 
     def callback(self, verts):
         ind = nonzero(points_inside_poly(self.xys, verts))[0]
         self.canvas.draw_idle()
         self.canvas.widgetlock.release(self.lasso)
         del self.lasso
+        del self.xys
         self.verts = verts
         self.ind = ind
 
