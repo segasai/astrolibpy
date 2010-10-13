287f95fc740df9a9ec80bbdaf7139b70d4a52ed3
koposov <koposov@8e70b0ce-f37c-11dd-afc9-ff9d66c6243e>
2010-10-13 13:27:17 +0000

    fix the comments in match_lists to be visible through ipython
    git-svn-id: https://astrolibpy.googlecode.com/svn/trunk@214 8e70b0ce-f37c-11dd-afc9-ff9d66c6243e

--- my_utils/match_lists.py
+++ my_utils/match_lists.py
@@ -22,15 +22,16 @@ from numpy import sin,cos,deg2rad,rad2deg,arcsin
 scipy_version  = float('.'.join(version.version.split('.')[0:2]))
 
 def match_lists(ra1, dec1, ra2, dec2, dist):
-	# crossmatches the list of objects (ra1,dec1) with
-	# another list of objects (ra2,dec2) with the dist matching radius
-	# the routine returns the distance to the neighbor and the list 
-	# of indices of the neighbor. Everything is in degrees.
-	# if no match is found the distance is NaN.
-	# Example 
-	# dist,ind=match(ra1,dec1,ra2,dec2)
-	# goodmatch_ind = numpy.isfinite(dist)
-	# plot(ra1[goodmatch_ind],ra2[ind][goodmatch_ind])
+	"""crossmatches the list of objects (ra1,dec1) with
+	another list of objects (ra2,dec2) with the dist matching radius
+	the routine returns the distance to the neighbor and the list 
+	of indices of the neighbor. Everything is in degrees.
+	if no match is found the distance is NaN.
+	Example: 
+	dist,ind=match(ra1,dec1,ra2,dec2)
+	goodmatch_ind = numpy.isfinite(dist)
+	plot(ra1[goodmatch_ind],ra2[ind][goodmatch_ind])
+	"""
 	cosd = lambda x : cos(deg2rad(x))
 	sind = lambda x : sin(deg2rad(x))
 	mindist = 2 * sind(dist/2)	
