ca255c612a1209272dce34f651d4e7c5f9dc7889
koposov <koposov@8e70b0ce-f37c-11dd-afc9-ff9d66c6243e>
2010-10-25 00:22:03 +0000

    add new option to window_func + add some documentation
    git-svn-id: https://astrolibpy.googlecode.com/svn/trunk@218 8e70b0ce-f37c-11dd-afc9-ff9d66c6243e

--- my_utils/window_func.py
+++ my_utils/window_func.py
@@ -1,12 +1,17 @@
 import scipy,numpy
 
-def window_func(x,y,func,xmin=None,xmax=None,nbin=100):
+def window_func(x,y,func,xmin=None,xmax=None,nbin=100, empty=False ):
 	"""This function does compute a user-supplied function 
 	on the subsets of y grouped by values of x
 	E.g. imagine that you have x from 0 to 100 and you want
 	to know the mean values of y for 0<x<10, 10<x<2- etc..
 	In that case you need to do
-	window_func(x,y,lambda x: x.mean(),0,100,nbin=10)
+	xbin,funcy,nperbin=window_func(x,y,lambda x: x.mean(),0,100,nbin=10)
+	where xbin is the array with the centers of bins, 
+	funcy is the func -- evaluated for y where x is within the appropriate bin
+	and nperbin is the number of points in the appropriate bin
+	empty keyword is needed if you want to retrieve the function evaluation 
+	in empty bins too, otherwise they aren't returned at all
 	"""
 	
 	if xmin is None:
@@ -15,7 +20,14 @@ def window_func(x,y,func,xmin=None,xmax=None,nbin=100):
 		xmax = x.max()
 	hh,loc=scipy.histogram(x,range=(xmin,xmax),bins=nbin)
 	inds= numpy.digitize(x,loc)
-	hh=hh*0.
+	mask=numpy.zeros(len(hh),bool)
+	retv=hh*0.	
 	for i in range(nbin):
-		hh[i]=func(y[inds==i])
-	return (loc[1:]+loc[:-1])*0.5,hh
+		cury=y[inds==i]
+		if len(cury)>0 or empty:
+			retv[i]=func(cury)
+		mask[i]=len(cury)>0
+	retx = (loc[1:]+loc[:-1])*0.5 # middle of the bin
+	if empty:
+		mask |= True
+	return retx[mask], retv[mask],hh[mask]
