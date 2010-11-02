76cfe98a4e990785e831f1e77b2a17e7a93319ed
koposov <koposov@8e70b0ce-f37c-11dd-afc9-ff9d66c6243e>
2010-11-02 15:33:01 +0000

    allow some customization of the get_dss call
    git-svn-id: https://astrolibpy.googlecode.com/svn/trunk@221 8e70b0ce-f37c-11dd-afc9-ff9d66c6243e

--- my_utils/get_dss.py
+++ my_utils/get_dss.py
@@ -1,9 +1,16 @@
 import urllib2,StringIO,aplpy,pyfits
 import matplotlib.pyplot  as plt
-def get_dss(ra,dec):
-	url='http://archive.stsci.edu/cgi-bin/dss_search?v=poss2ukstu_red&r=%fd&d=%fd&e=J2000&h=15.0&w=15.0&f=fits&c=none&fov=NONE&v3='%(ra,dec)
+def get_dss(ra, dec, survey='all', radius = 15, debug=False):
+	"""
+	radius is in arcmins
+	survey: 'all', 'poss2ukstu', 'dss1'.. 
+	"""
+	#survey='poss2ukstu'
+	url='http://archive.stsci.edu/cgi-bin/dss_search?v=%s&r=%fd&d=%fd&e=J2000&h=%f&w=%f&f=fits&c=none&fov=NONE&v3='%(survey,ra,dec,radius,radius)
 	
 	response = urllib2.urlopen(url)
+	if debug:
+		print url
 	html = response.read()
 	f=StringIO.StringIO(html)
 	
