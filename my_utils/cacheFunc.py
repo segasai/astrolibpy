f34878863313074d67e64c15d9b8c1dac5a18f05
koposov <koposov@8e70b0ce-f37c-11dd-afc9-ff9d66c6243e>
2011-01-13 15:27:54 +0000

    add a function which caches the function calls. It cannot be used as a decorator yet, but still can be useful
    git-svn-id: https://astrolibpy.googlecode.com/svn/trunk@255 8e70b0ce-f37c-11dd-afc9-ff9d66c6243e

---
+++ my_utils/cacheFunc.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+
+class cacheFunc:
+	""" class to cache function calls
+	E.g.
+	>> def func(a,b=3):
+	>>	# do something 
+	>>	print 'Called', a,b
+	>>	return a+b
+		
+	>> funcC = cacheFunc(func)
+	>> print funcC(2,b=3)
+	Called 2 3
+	5
+	>> print funcC(3,b=4)
+	Called 3 4
+	7
+	>> print funcC(2,b=3)
+	5
+	"""
+	def __init__(self, func,maxn=10):
+		self.hash={}
+		self.l=deque()
+		self.maxn=maxn
+		self.func=func
+	
+	def __call__(self,*args, **kw):
+		key =  args+tuple(kw.iteritems())
+		if self.hash.get(key) is not None:
+			pass
+		else:
+			self.hash[key]=self.func(*args, **kw)
+			self.l.append(key)
+			if len(self.l)>self.maxn:
+				delarg = self.l.popleft()
+				del self.hash[delarg]
+				del delarg
+		return self.hash[key]
+								
