24a8aa781a70075179691665cb814788fbe16b70
koposov <koposov@8e70b0ce-f37c-11dd-afc9-ff9d66c6243e>
2011-03-18 01:01:48 +0000

    add a special plotting module useful (to me in interactive sessions). The plotting commands from idlplotInd accept ind keyword, which is then applied to all arguments
    git-svn-id: https://astrolibpy.googlecode.com/svn/trunk@312 8e70b0ce-f37c-11dd-afc9-ff9d66c6243e

---
+++ my_utils/idlplotInd.py
@@ -0,0 +1,56 @@
+import idlplot
+
+def tvhist2d(a,b,*args,**kw):
+	ind = kw.get('ind')
+	
+	if ind is None:
+		idlplot.tvhist2d(a,b,*args,**kw)
+	else:
+		weights = kw.get('weights')
+		if weights is not None:
+			kw['weights']=kw['weights'][ind]
+		del kw['ind']
+		idlplot.tvhist2d(a[ind],b[ind],*args,**kw)
+
+def plothist(a,*args,**kw):
+	ind = kw.get('ind')
+	
+	if ind is None:
+		idlplot.plothist(a,*args,**kw)
+	else:
+		weights = kw.get('weights')
+		if weights is not None:
+			kw['weights']=kw['weights'][ind]
+		del kw['ind']
+		idlplot.plothist(a[ind],*args,**kw)
+
+def plot(a,b,*args,**kw):
+	ind = kw.get('ind')
+	
+	if ind is None:
+		idlplot.plot(a,b,*args,**kw)
+	else:
+		del kw['ind']
+		idlplot.plot(a[ind],b[ind],*args,**kw)
+
+def oplot(a,b,*args,**kw):
+	ind = kw.get('ind')
+	
+	if ind is None:
+		idlplot.oplot(a,b,*args,**kw)
+	else:
+		del kw['ind']
+		idlplot.oplot(a[ind],b[ind],*args,**kw)
+
+
+def ploterror(a,b,c,*args,**kw):
+	ind = kw.get('ind')
+	
+	if ind is None:
+		idlplot.ploterror(a,b,c,*args,**kw)
+	else:
+		del kw['ind']
+		l = len(args)
+		for i in range(l):
+			args[i]=args[i][ind]
+		idlplot.ploterror(a[ind],b[ind],c[ind],*args,**kw)
