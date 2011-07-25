d76c57aff272e96336b71f94d9f1a0fc291f77c4
koposov <koposov@8e70b0ce-f37c-11dd-afc9-ff9d66c6243e>
2011-07-25 13:32:08 +0000

    update the plot, oplot in idlplotInd module to do plotting when only y-vector is specified, but no x
    git-svn-id: https://astrolibpy.googlecode.com/svn/trunk@345 8e70b0ce-f37c-11dd-afc9-ff9d66c6243e

--- my_utils/idlplotInd.py
+++ my_utils/idlplotInd.py
@@ -1,5 +1,12 @@
 import idlplot
 
+""" 
+This module is a set wrappers around idlplot designed to make 
+the plots of subsets of the data: e.g.
+plot(x,y,ind=ind)
+instead of plot(x[ind],y[ind])
+  
+"""
 def tvhist2d(a,b,*args,**kw):
 	ind = kw.get('ind')
 	
@@ -24,23 +31,30 @@ def plothist(a,*args,**kw):
 		del kw['ind']
 		idlplot.plothist(a[ind],*args,**kw)
 
-def plot(a,b,*args,**kw):
+def plot(a, b=None, **kw):
 	ind = kw.get('ind')
 	
 	if ind is None:
-		idlplot.plot(a,b,*args,**kw)
+		idlplot.plot(a,b,**kw)
 	else:
 		del kw['ind']
-		idlplot.plot(a[ind],b[ind],*args,**kw)
+		if b is not None:
+			idlplot.plot(a[ind], b[ind], **kw)
+		else:
+			idlplot.plot(a[ind], None, **kw)
+			
 
-def oplot(a,b,*args,**kw):
+def oplot(a, b=None, **kw):
 	ind = kw.get('ind')
 	
 	if ind is None:
-		idlplot.oplot(a,b,*args,**kw)
+		idlplot.oplot(a, b, **kw)
 	else:
 		del kw['ind']
-		idlplot.oplot(a[ind],b[ind],*args,**kw)
+		if b is not None:
+			idlplot.oplot(a[ind], b[ind], **kw)
+		else:
+			idlplot.oplot(a[ind], **kw)
 
 
 def ploterror(a,b,c,*args,**kw):
