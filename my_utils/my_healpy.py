421252d159675c9a6c9586d99895db3d058932ae
Sergey Koposov <math@fortune>
2013-04-02 17:47:09 +0100

    add a new healpy calculation file

---
+++ my_utils/my_healpy.py
@@ -0,0 +1,10 @@
+import healpy,numpy as np
+
+def radec2ipix(nside,ra,dec,nest=True):
+	_,__=np.pi/2-np.deg2rad(dec),np.deg2rad(ra)
+	return healpy.ang2pix(nside,_,__,nest=nest)
+
+def pix2radec(nside,pix,nest=True):
+	__,_ = healpy.pix2ang(nside,pix,nest=nest)
+	ra,dec=np.rad2deg(_),np.rad2deg(np.pi/2-__)
+	return ra,dec
