37729579dd3ac7babb64360292af891bf6077f77
Sergey Koposov <math@fortune>
2015-07-06 12:38:51 +0100

    add a rebin function

---
+++ my_utils/rebin.py
@@ -0,0 +1,17 @@
+import scipy.signal,numpy as np
+
+def rebin(arr, shape1):
+	# rebins the array too the new shape by summing the elements
+	# it is assumed that in each dimension the resizing is by an integer
+	# factor
+	shape0 = arr.shape 
+	rats = np.array([_//__ for _, __ in zip (shape0,shape1)])
+	for r, s0, s1 in zip(rats,shape0,shape1):
+		if r*s1!=s0:
+			raise Exception('Wrong array sizes')
+	R = rats.prod()
+	conv=scipy.signal.convolve(arr,
+		np.ones(dtype=np.int64,shape=rats),'valid')
+	ret = conv[tuple([slice(0,None,_) for _ in rats])]
+	return ret
+	
