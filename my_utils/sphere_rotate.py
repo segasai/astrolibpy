d128b28893b5e6367beca133fa6275353f92fe6e
Sergey Koposov <math@fortune>
2016-04-21 14:21:17 +0100

    speed up the sphere_rotate

--- my_utils/sphere_rotate.py
+++ my_utils/sphere_rotate.py
@@ -1,13 +1,25 @@
 import numpy
+import numexpr
 from cv_coord import cv_coord
 
+def torect(ra,dec):
+	x=numexpr.evaluate('cos(ra/57.295779513082323)*cos(dec/57.295779513082323)')
+	y=numexpr.evaluate('sin(ra/57.295779513082323)*cos(dec/57.295779513082323)')
+	z=numexpr.evaluate('sin(dec/57.295779513082323)')
+	return x,y,z
+
+def fromrect(x,y,z):
+	ra=numexpr.evaluate('arctan2(y,x)*57.295779513082323')
+	dec=numexpr.evaluate('57.295779513082323*arctan2(z,sqrt(x**2+y**2))')
+	return ra,dec
 
 def sphere_rotate(ra, dec, rapol, decpol, ra0, revert=False):
 	""" rotate ra,dec to a new spherical coordinate system where the pole is 
 		at rapol,decpol and the zeropoint is at ra=ra0 
 		revert flag allows to reverse the transformation
 	"""
-	x,y,z=cv_coord(ra,dec,dec*0+1,fr='sph',to='rect',degr=True)
+
+	x,y,z=torect(ra,dec)
 
 	tmppol=cv_coord(rapol,decpol,1,degr=True,fr='sph',to='rect') #pole axis
 	tmpvec1=cv_coord(ra0,0,1,degr=True,fr='sph',to='rect') #x axis
@@ -18,13 +30,17 @@ def sphere_rotate(ra, dec, rapol, decpol, ra0, revert=False):
 	tmpvec2=numpy.cross(tmppol,tmpvec1) # y axis 
 
 	if not revert:
-		xnew=x*tmpvec1[0]+y*tmpvec1[1]+z*tmpvec1[2]
-		ynew=x*tmpvec2[0]+y*tmpvec2[1]+z*tmpvec2[2]
-		znew=x*tmppol[0] +y*tmppol[1] +z*tmppol[2]
+		Axx,Axy,Axz=tmpvec1
+		Ayx,Ayy,Ayz=tmpvec2
+		Azx,Azy,Azz=tmppol
 	else:
-		xnew=x*tmpvec1[0]+y*tmpvec2[0]+z*tmppol[0]
-		ynew=x*tmpvec1[1]+y*tmpvec2[1]+z*tmppol[1]
-		znew=x*tmpvec1[2]+y*tmpvec2[2]+z*tmppol[2]
+		Axx,Ayx,Azx=tmpvec1
+		Axy,Ayy,Azy=tmpvec2
+		Axz,Ayz,Azz=tmppol
+	xnew = numexpr.evaluate('x*Axx+y*Axy+z*Axz')
+	ynew = numexpr.evaluate('x*Ayx+y*Ayy+z*Ayz')
+	znew = numexpr.evaluate('x*Azx+y*Azy+z*Azz')
+	
 	del x,y,z
-	tmp=cv_coord(xnew,ynew,znew,to='sph',fr='rect',degr=True)
+	tmp = fromrect(xnew,ynew,znew)
 	return (tmp[0],tmp[1])
