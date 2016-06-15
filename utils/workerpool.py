e29106e651eb7ccbf0acad2320c922aeaa84a796
Sergey Koposov <math@fortune>
2016-06-15 16:57:17 +0100

    fix the worker pool to accept args as well as keywords

--- utils/workerpool.py
+++ utils/workerpool.py
@@ -1,23 +1,23 @@
 import multiprocessing as mp, numpy as np
 import signal
 
-def doit(func, queuein, queout, kw):	
+def doit(func, queuein, queout, args, kw):	
 	signal.signal(signal.SIGINT, signal.SIG_IGN)
 	while True:
 		inp = queuein.get()
 		if inp is None:
 			break
 		i, p = inp
-		res = func(p, **kw)
+		res = func(p, *args, **kw)
 		queout.put((i, res))
 
 class pool:
-	def __init__(self, func, kw={}, nthreads=1):
+	def __init__(self, func, args=None, kw={}, nthreads=1):
 		self.nthreads = nthreads
 		self.queuein = mp.Queue()
 		self.queueout = mp.Queue()
 		self.procs = [mp.Process(target=doit, name='xpool_%d'%i,
-				args=(func, self.queuein, self.queueout, kw))
+				args=(func, self.queuein, self.queueout, args, kw))
 					for i in range(nthreads) ]
 		[_.start() for _ in self.procs]
 		self.stage = {}
@@ -46,7 +46,7 @@ class pool:
 			iret, ret = self.queueout.get()
 			return iret, ret
 
-	def map(self, params):
+	def map0(self, params):
 		for i, p in enumerate(params):
 			self.queuein.put((i, p))
 		ret = [None] * len(params)
@@ -54,6 +54,9 @@ class pool:
 			resi, res = self.queueout.get()
 			ret[resi] = res
 		return ret
+
+	def map(self, params):
+		return self.map0(params)
 		
 	def __del__(self):
 		self.join()
