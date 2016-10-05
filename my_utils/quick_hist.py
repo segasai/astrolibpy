44a5176a58126f290a150d48188acb834b06ae95
Sergey Koposov <math@fortune>
2016-10-05 20:43:08 +0100

    finally migrate quick_hist from weave to cffi

--- my_utils/quick_hist.py
+++ my_utils/quick_hist.py
@@ -1,23 +1,124 @@
 # Copyright (C) 2009-2010 Sergey Koposov
 # This file is part of astrolibpy
 #
-#    astrolibpy is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#	 astrolibpy is free software: you can redistribute it and/or modify
+#	 it under the terms of the GNU General Public License as published by
+#	 the Free Software Foundation, either version 3 of the License, or
+#	 (at your option) any later version.
 #
-#   astrolibpy is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#	astrolibpy is distributed in the hope that it will be useful,
+#	 but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	 GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with astrolibpy.  If not, see <http://www.gnu.org/licenses/>.
+#	 You should have received a copy of the GNU General Public License
+#	 along with astrolibpy.	 If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import print_function
 from functools import reduce
-
+import warnings
 import numpy as np
+import hashlib
+import importlib
+
+def _getcode():
+	cdef="""
+	void hister(
+		double cur_range0,
+		double cur_range1,
+		int cur_nbins,
+		int cur_mult,
+		double *cur_arr,
+		int64_t *poss,
+		int64_t *ind,
+		int nx);
+
+	void adder_now(
+		int64_t* res,
+		int64_t* poss,
+		int newlen);
+
+	void adder_wei(
+		double* res,
+		int64_t* poss,
+		double* weights,
+		int newlen);
+
+	"""								
+
+	src="""
+	static void hister(
+		double cur_range0,
+		double cur_range1,
+		int cur_nbins,
+		int cur_mult,
+		double *cur_arr,
+		int64_t *poss,
+		int64_t *ind,
+		int nx)
+		{
+			int64_t i, cur_pos;
+			double curfac = cur_nbins * 1./ (cur_range1- cur_range0);
+			for(i=0;i<nx;i++)
+			{
+				cur_pos = (int)floor( ( cur_arr[i]-cur_range0) * curfac);
+				if ((cur_pos>=0 ) && (cur_pos<cur_nbins))
+				{
+					poss[i] = poss[i] + cur_pos * cur_mult;
+				}
+				else
+				{
+					ind[i]=0;
+				}    		 
+				 
+			}
+		}
+
+	static void adder_now(
+		int64_t* res,
+		int64_t* poss,
+		int newlen)
+	{
+		int i;
+		for (i=0; i<newlen ;i++)
+		{
+			res[poss[i]] = res[poss[i]] + 1;
+		}
+	}
+
+	static void adder_wei(
+		double* res,
+		int64_t* poss,
+		double* weights,
+		int newlen)
+	{
+		int i;
+		for (i=0; i<newlen ;i++)
+		{
+			res[poss[i]] = res[poss[i]]+weights[i];
+		}
+	}
+		
+	"""
+	return cdef, src
+
+def _hasher(cdef, src):
+	md5 = hashlib.md5((cdef+src).encode('utf-8')).hexdigest()[:6]
+	return md5
+
+def _getlibname():
+	hash = _hasher(*_getcode())
+	return '_quick_hist_'+hash
+
+def _buildlib():
+	cdef, src = _getcode()
+	libname = _getlibname()
+	from cffi import FFI
+	ffibuilder = FFI()
+	ffibuilder.cdef(cdef)
+	ffibuilder.set_source(libname, src)
+	ffibuilder.compile(verbose=True)
+
 
 def quick_hist(arrs, range=None, nbins=None, weights=None, getPos=False):
 	"""
@@ -26,119 +127,120 @@ def quick_hist(arrs, range=None, nbins=None, weights=None, getPos=False):
 	> xs=np.random.uniform(size=100); ys= np.random.uniform(size=100)
 	> hh = quick_hist((xs,ys), range=[(0,1),(0,1)], nbins=[20,10])
 	Arguments:
-		arr -- tuple of N-arrays
-		range -- list of tuples of ranges
-		nbins -- list of numbers of bins 
+			arr -- tuple of N-arrays
+			range -- list of tuples of ranges
+			nbins -- list of numbers of bins 
 	Keywords:
-		weights -- weighting for the histogram
-		getPos -- return the 1D vector of the positions within the histogram
-					(-1 if the point is outside the range)
+			weights -- weighting for the histogram
+			getPos -- return the 1D vector of the positions within the histogram
+									(-1 if the point is outside the range)
 	"""
 	nd = len(arrs)
 	if range is None:
-		range=[]
+		range = []
 		for i in np.arange(nd):
-			range.append((arrs[0].min(),arrs[0].max()))
+			range.append((arrs[0].min(), arrs[0].max()))
 	if nbins is None:
-		nbins = [10]*nd
-				
-	if len(nbins)!=nd:
-		raise ValueError('The array of nbins MUST have the same length as the number of input data vectors')
-	if len(range)!=nd:
-		raise ValueError('The array of ranges MUST have the same length as the number of input data vectors')
+		nbins = [10] * nd
+
+	if len(nbins) != nd:
+		raise ValueError(
+			'The array of nbins MUST have the same length as the number of input data vectors')
+	if len(range) != nd:
+		raise ValueError(
+			'The array of ranges MUST have the same length as the number of input data vectors')
 
 	nx = len(arrs[0])
 	for curarr in arrs:
-		if len(curarr)!=nx:
+		if len(curarr) != nx:
 			raise ValueError('All the input arrays MUST have the same length!')
 	if weights is not None:
-		if len(weights)!=nx:
-			raise ValueError('The weights array MUST have the same length as the input arrays')
-	# convert all the bins into integers 
-	nbins = [ int(_tmp) for _tmp in nbins]
+		if len(weights) != nx:
+			raise ValueError(
+				'The weights array MUST have the same length as the input arrays')
+	# convert all the bins into integers
+	nbins = [int(_tmp) for _tmp in nbins]
 
 	poss = np.zeros((nx,), dtype=np.int64)
-
-	ind = np.ones_like(arrs[0]).astype(bool)
+	ind = np.ones((nx,), dtype=np.int64)
 	nbins_rev = nbins + []
 	nbins_rev.reverse()
 	mults = (reduce(lambda x, y: x + [y * x[-1]], nbins_rev, [1]))[:-1]
-	mults.reverse()	
+	mults.reverse()
 	assert(poss.flags['C_CONTIGUOUS'])
 	assert(ind.flags['C_CONTIGUOUS'])
+	slow = False
+	modname = _getlibname()
 
 	for i in np.arange(nd):
-		cur_arr = np.ascontiguousarray(arrs[i],dtype=np.float64)
+		cur_arr = np.ascontiguousarray(arrs[i], dtype=np.float64)
 		cur_range0 = float(range[i][0])
 		cur_range1 = float(range[i][1])
-		cur_nbins = nbins[i]
-		cur_mult = mults[i]
-		code1 = """
-		int i, cur_pos;
-		double curfac = cur_nbins * 1./ (cur_range1-cur_range0);
-		for (i=0; i<nx; i++)
-		{
-			cur_pos = (int)floor( ( cur_arr(i)-cur_range0) * curfac);
-
-			if ((cur_pos>=0 ) && (cur_pos<cur_nbins))
-			{
-				poss(i)=poss(i)+cur_pos*cur_mult;
-			}
-			else
-			{
-				ind(i)=false;
-			}
-		}"""
+		cur_nbins = int(nbins[i])
+		cur_mult = int(mults[i])
 		try:
-			import scipy.weave, scipy.weave.converters
-			scipy.weave.inline(code1, ['cur_range0', 'cur_range1', 'cur_nbins','poss','ind','nx','cur_arr','cur_mult'],
-				type_converters=scipy.weave.converters.blitz)
+			try:
+				M = importlib.import_module(modname)
+				#from _quick_hist import ffi, lib
+			except:
+				_buildlib()
+				M = importlib.import_module(modname)
+			poss_ffi = M.ffi.cast('int64_t *', M.ffi.from_buffer(poss))
+			ind_ffi = M.ffi.cast('int64_t *', M.ffi.from_buffer(ind))
+			cur_arr_ffi = M.ffi.cast('double *', M.ffi.from_buffer(cur_arr))
+
+			M.lib.hister(cur_range0, cur_range1, cur_nbins,
+					   cur_mult, cur_arr_ffi,
+					   poss_ffi, ind_ffi, nx)
 		except:
-			print( "Sorry the compiled version didn't work :(")
-			cur_pos = (cur_arr - cur_range0) * (cur_nbins * 
-									1. / (cur_range1 - cur_range0))
+			warnings.warn("""Sorry the compiled version didn't work :(, executing slower version\nYou need to do execute python _quick_hist_build.py to """)
+			slow = False
+			cur_pos = (cur_arr - cur_range0) * (cur_nbins *
+												1. / (cur_range1 - cur_range0))
 			cur_pos = np.floor(cur_pos).astype(np.int64)
-			ind &= ((cur_pos >= 0) & ( cur_pos < cur_nbins))
+			ind = ind * ((cur_pos >= 0) & (cur_pos < cur_nbins))
 			poss += cur_pos * cur_mult
+
+	ind = ind.astype(bool)
 
 	poss = poss[ind]
 	newlen = len(poss)
-	if weights is None:
-		weights_str = '1'
-	else:
-		weightsind = np.ascontiguousarray(weights[ind], dtype=np.float64)
-		weights_str = 'weightsind(i)'
-	if not getPos:	
-		del ind
-	res = np.zeros(np.array(nbins, dtype=np.int64).prod())
-	assert(res.flags['C_CONTIGUOUS'])
 
-	code = """
-	int i;
-	for (i=0; i<newlen; i++)
-	{
-		res(poss(i)) = res(poss(i)) + %s;
-	}"""%weights_str
-	try:
-		import scipy.weave, scipy.weave.converters
-		if weights is None:
-			scipy.weave.inline(code, ['res', 'poss', 'newlen'],
-				type_converters=scipy.weave.converters.blitz)
+
+	nret = np.array(nbins, dtype=np.int64).prod()
+
+	if weights is not None:
+		res = np.zeros(nret, dtype=np.float64)
+		weightsind = np.ascontiguousarray(weights[ind], dtype=np.float64)
+	else:
+		res = np.zeros(nret, dtype=np.int64)
+
+	if not getPos:
+		del ind
+
+	assert(res.flags['C_CONTIGUOUS'])
+	assert(poss.flags['C_CONTIGUOUS'])
+
+	if not slow:
+		poss_ffi = M.ffi.cast('int64_t *', M.ffi.from_buffer(poss))
+
+		if weights is not None:
+			weightsind_ffi = M.ffi.cast('double *', M.ffi.from_buffer(weightsind))
+			res_ffi = M.ffi.cast('double *', M.ffi.from_buffer(res))
+			M.lib.adder_wei(res_ffi, poss_ffi, weightsind_ffi, newlen)
 		else:
-			scipy.weave.inline(code, ['res', 'poss', 'newlen','weightsind'],
-				type_converters=scipy.weave.converters.blitz)			
-	except Exception:
-		print ( "Sorry the compiled version didn't work :(")
+			res_ffi = M.ffi.cast('int64_t *', M.ffi.from_buffer(res))
+			M.lib.adder_now(res_ffi, poss_ffi, newlen)
+	else:
 		if weights is None:
 			for i in np.arange(len(poss)):
-				res[poss[i]]+=1
+				res[poss[i]] += 1
 		else:
 			for i in np.arange(len(poss)):
-				res[poss[i]]+=weights[i]
+				res[poss[i]] += weights[i]
 	if not getPos:
 		return res.reshape(nbins)
 	else:
-		H = np.zeros(len(ind),dtype=np.int64)-1
+		H = np.zeros(len(ind), dtype=np.int64) - 1
 		H[ind] = poss
-		return res.reshape(nbins),H	
-		
+		return res.reshape(nbins), H
