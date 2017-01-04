c6f7ea0ff1449249c756f82bec5e90064ea962cb
Sergey Koposov <math@fortune>
2017-01-04 00:05:30 +0000

    make clicker py3 compatible

--- my_utils/clicker.py
+++ my_utils/clicker.py
@@ -1,17 +1,17 @@
-
+from __future__ import print_function
 
 def clicker(fig,xobj = None):
 	cid = None 
 	def onclick(event):
 		try:
-			print 'cid=%s button=%d, x=%d, y=%d, xdata=%f, ydata=%f'%(cid,
-				event.button, event.x, event.y, event.xdata, event.ydata)
+			print ('cid=%s button=%d, x=%d, y=%d, xdata=%f, ydata=%f'%(cid,
+				event.button, event.x, event.y, event.xdata, event.ydata))
 			if xobj is not None:
 				if isinstance (xobj,dict):
 					xobj['x']=event.xdata
 					xobj['y']=event.ydata
 		except:
-			print 'printing failed'
+			print( 'printing failed')
 		event.canvas.mpl_disconnect(cid)
 
 	cid = fig.canvas.mpl_connect('button_press_event', onclick)
@@ -21,14 +21,14 @@ def clicker_multi(fig):
 	res = [] 
 	def onclick(event):
 		try:
-			print 'cid=%s button=%d, x=%d, y=%d, xdata=%f, ydata=%f'%(cid,
-					event.button, event.x, event.y, event.xdata, event.ydata)
+			print( 'cid=%s button=%d, x=%d, y=%d, xdata=%f, ydata=%f'%(cid,
+					event.button, event.x, event.y, event.xdata, event.ydata))
 			if event.button==3:
 				event.canvas.mpl_disconnect(cid)
 			else:
 				res.append((event.xdata,event.ydata))
 		except:
-			print 'printing failed'
+			print( 'printing failed')
 
 	cid = fig.canvas.mpl_connect('button_press_event', onclick)
 	return res	
