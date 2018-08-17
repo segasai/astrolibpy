50003ce8b711ac49cb07032e7a25da911a43ddf2
Sergey Koposov <skoposov@cmu.edu>
2018-08-17 18:58:59 -0400

    use zeep module instead of suds to access CDS's SOAP services

--- my_utils/resolve.py
+++ my_utils/resolve.py
@@ -1,34 +1,40 @@
 import sys
-from suds.client import Client
+#from suds.client import Client
+from zeep import Client
 from lxml import etree
 
 def resolve(objectName):
-	"""
-	Resolve the object by name using CDS
-	Example:
-	>> resolve.resolve('M31')
-	(10.684708329999999, 41.268749999999997)
+    """    R
+esolve the object by name using CDS
+    Example:
+    >> resolve.resolve('M31')
+    (10.684708329999999, 41.268749999999997)
 
-	Requires the following modules:
-		suds, lxml
-	"""
-	url = 'http://cdsws.u-strasbg.fr/axis/services/Sesame?wsdl'
-	client = Client(url)    
-	xml=client.service.SesameXML(objectName)           
-	tree = etree.fromstring(xml.encode('utf-8'))
-	# take the first resolver
-	pathRa = tree.xpath('/Sesame/Target/Resolver[1]/jradeg')
-	pathDec = tree.xpath('/Sesame/Target/Resolver[1]/jdedeg')
-	if len(pathRa)==0:
-		return []
-	ra=float(pathRa[0].text)
-	dec=float(pathDec[0].text)
-	return ra,dec
+    Requires the following modules:
+        suds, lxml
+    """
+    url = 'http://cdsws.u-strasbg.fr/axis/services/Sesame?wsdl'
+    client = Client(url)    
+    xml=client.service.SesameXML(objectName)           
+    tree = etree.fromstring(xml.encode('utf-8'))
+    # take the first resolver
+    success=True
+    for i in range(4):
+        pathRa = tree.xpath('/Sesame/Target/Resolver[%d]/jradeg'%i)
+        pathDec = tree.xpath('/Sesame/Target/Resolver[%d]/jdedeg'%i)
+        if len(pathRa)!=0:
+            success=True
+            break
+    if not success:
+        return []
+    ra=float(pathRa[0].text)
+    dec=float(pathDec[0].text)
+    return ra,dec
 
 
 if __name__=='__main__':
-	res = resolve(sys.argv[1])
-	if len(res)>0:
-		print (res[0],res[1])
-	else:
-		print ('Not found')
+    res = resolve(sys.argv[1])
+    if len(res)>0:
+        print (res[0],res[1])
+    else:
+        print ('Not found')
