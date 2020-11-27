aca0bf772057127edb3d4b7f4ac7058b71d6b605
Sergey E. Koposov <skoposov@ed.ac.uk>
2020-11-27 14:31:58 +0000

    add alias name for the crossmatcher

--- my_utils/crossmatcher.py
+++ my_utils/crossmatcher.py
@@ -15,10 +15,11 @@ def doit(tabname,
          db=None,
          user=None,
          password=None,
-         asDict=False):
+         asDict=False,
+         tab_alias='tt'):
     """
     Performs the nearest neighbor crossmatch within specified radius
-    with the remote table the input is given by ra,dec columns 
+    with the remote table the input is given by ra,dec columns
 
     Parameters:
     -----------
@@ -30,22 +31,25 @@ def doit(tabname,
     dec : numpy
         The numpy array with declinations
     colstring: string
-        The comma sepated list of columns that you want to retrieve	
+        The comma sepated list of columns that you want to retrieve
     radeccols: tuple (optional)
         The tuple of two strings with the name of ra,dec columns in
         the remote table. Default ('ra','dec')
     rad: float (optional)
         The cross-match radius in arcseconds (default 1)
-    extra: string allows you to specify and additional SQL Where condition 
+    extra: string allows you to specify and additional SQL Where condition
+    tab_alias: string
+        The alias for the table that you are crossmatching against, so you can
+        refer to its columns
 
     Example:
     --------
     > ra = np.arange(10)
     > dec = np.arange(10)+5
     > gmag,rmag= crossmatcher.doit('sdssdr9.phototag', ra,dec,
-			'psfmag_g,psfmag_r', rad=2.)
-	
-	"""
+           'psfmag_g,psfmag_r', rad=2.)
+
+    """
     racol, deccol = radeccols
     if extra is None:
         extra = 'true'
@@ -53,21 +57,19 @@ def doit(tabname,
     RES = sqlutilpy.local_join(
         str.format(
             """
-		select {colstring} from 
-                  ( select * from mytable order by q3c_ang2ipix({your_ra},{your_dec})
-                  ) as m 
-			left join lateral (select * from {tabname} as s 
-					where 
-					q3c_join(m.{your_ra}, m.{your_dec}, 
-							s.{racol}, s.{deccol}, {rad}/3600.) 
-					and {extra}
-					order by 
-			q3c_dist(m.{your_ra}, m.{your_dec}, 
-						s.{racol},s.{deccol}) asc limit 1) as tt 
-				on true order by xid """, **locals()),
+            select {colstring} from
+                  ( select * from mytable order by
+            q3c_ang2ipix({your_ra},{your_dec})
+                  ) as m
+            left join lateral (select * from {tabname} as s where
+            q3c_join(m.{your_ra}, m.{your_dec},
+            s.{racol}, s.{deccol}, {rad}/3600.) and {extra}
+                order by q3c_dist(m.{your_ra}, m.{your_dec},
+                s.{racol},s.{deccol}) asc limit 1) as {tab_alias}
+            on true order by xid """, **locals()),
         'mytable', (ra, dec, np.arange(len(ra))), (your_ra, your_dec, 'xid'),
-        preamb=
-        'set enable_seqscan to off; set enable_mergejoin to off; set enable_hashjoin to off;',
+        preamb=('set enable_seqscan to off;' + 'set enable_mergejoin to off;' +
+                'set enable_hashjoin to off;'),
         host=host,
         db=db,
         user=user,
