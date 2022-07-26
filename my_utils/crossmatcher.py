09d19aa034958767178370d81d287db0064ba7d8
Sergey E. Koposov <skoposov@ed.ac.uk>
2022-07-26 20:51:39 +0100

    first implementation of xmatching by id

--- my_utils/crossmatcher.py
+++ my_utils/crossmatcher.py
@@ -89,3 +89,83 @@ def doit(tabname,
         asDict=asDict,
         conn=conn)
     return RES
+
+
+def doit_by_key(tabname,
+                keys,
+                colstring,
+                key_col=None,
+                rad=1.,
+                extra=None,
+                yourkeycol='id',
+                tab_alias='tt',
+                host=None,
+                port=None,
+                db=None,
+                user=None,
+                password=None,
+                asDict=False,
+                conn=None,
+                preamb=None):
+    """
+    Performs the crossmatch by id
+    with the remote table 
+
+    Parameters:
+    -----------
+
+    tabname: string
+        The name of the table to crossmatch against
+    keys: numpy
+        The numpy array with ids that will be used for matching
+    colstring: string
+        The comma sepated list of columns that you want to retrieve
+    radeccols: tuple (optional)
+        The tuple of two strings with the name of ra,dec columns in
+        the remote table. Default ('ra','dec')
+    rad: float (optional)
+        The cross-match radius in arcseconds (default 1)
+    extra: string allows you to specify and additional SQL Where condition
+    tab_alias: string
+        The alias for the table that you are crossmatching against, so you can
+        refer to its columns
+    host,port,db,user,password: string
+        The connection parameters to the DB if needed
+    asDict: bool
+        if True instead of returning a tuple a dictionary is returned
+    preamb: string (optional)
+        additional commands to be executed before the query
+    conn: connection object(optional)
+        An explicit connection to the DB can be provided
+
+    Example:
+    --------
+    > ra = np.arange(10)
+    > dec = np.arange(10)+5
+    > gmag,rmag= crossmatcher.doit('sdssdr9.phototag', ra,dec,
+           'psfmag_g,psfmag_r', rad=2.)
+
+    """
+    if extra is None:
+        extra = 'true'
+    preamb = '' or preamb
+    RES = sqlutilpy.local_join(
+        str.format(
+            """
+            select {colstring} from
+                  ( select * from mytable
+                  ) as m
+            left join lateral (select * from {tabname} as s where
+            m.id=s.{key_col} and {extra} limit 1) as {tab_alias}
+            on true order by xid """, **locals()),
+        'mytable', (keys, np.arange(len(keys))), ('id', 'xid'),
+        preamb=('set enable_seqscan to off;' + 'set enable_mergejoin to off;' +
+                'set enable_hashjoin to off;' + (preamb or '')),
+        host=host,
+        db=db,
+        user=user,
+        port=(port or 5432),
+        password=password,
+        asDict=asDict,
+        conn=conn)
+    return RES
