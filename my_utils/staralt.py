cc66f51611b4a618fca46cd73eea728a5f67ecaf
Sergey E. Koposov <skoposov@ed.ac.uk>
2022-11-24 20:26:41 +0000

    reformat

--- my_utils/staralt.py
+++ my_utils/staralt.py
@@ -15,48 +15,60 @@
 #    You should have received a copy of the GNU General Public License
 #    along with astrolibpy.  If not, see <http://www.gnu.org/licenses/>.
 
-import ephem,numpy as np
-
+import ephem
+import numpy as np
 import sphdist
 
-def getalt(ra, dec, yr, mon, day, hr, minu, sec=0, lon='-111:35:59', lat='31:57:12',
-		elev=2000, retSun=False, retDistMoon=False):
-	"""computes the altitude in degrees of a given object at the given utc time
-	ra dec in degress	
-	yr mon day hr minu in utc
-	lon lat are in degrees and lon is positive to the East
-	retSun means that you'll get height of the sun above the horizon
-	retDistMoon means that you'll also get the distance to the moon
-	"""
-	
-	obs = ephem.Observer();
-	obs.lon = lon # longitude 
-	obs.lat = lat #latitude
-	obs.elevation=elev;
-	fb = ephem.FixedBody();
-	fb._ra=np.deg2rad(ra);
-	fb._dec=np.deg2rad(dec)
-	obs.date=ephem.Date('%d/%02d/%d %d:%d:0'%(yr,mon,day,hr,minu))
-	fb.compute(obs);
-	alt = np.rad2deg(1*fb.alt)
-	az = np.rad2deg(1*fb.az);
-	if retSun:
-		fbSun = ephem.Sun();
-		fbSun.compute(obs);
-		altSun = np.rad2deg(1*(fbSun.alt))
-	if retDistMoon:
-		fbMoon = ephem.Moon();
-		fbMoon.compute(obs);
-		altMoon = np.rad2deg(1*fbMoon.alt)
-		azMoon = np.rad2deg(1*fbMoon.az);
-		distMoon = sphdist.sphdist(az,alt,azMoon,altMoon)
-	if retSun or retDistMoon:
-		ret = [alt]
-	else:	
-		ret = alt
-	if retSun:
-		ret.append(altSun)
-	if retDistMoon:
-		ret.append(distMoon)
-	return ret
-	
+
+def getalt(ra,
+           dec,
+           yr,
+           mon,
+           day,
+           hr,
+           minu,
+           sec=0,
+           lon='-111:35:59',
+           lat='31:57:12',
+           elev=2000,
+           retSun=False,
+           retDistMoon=False):
+    """
+    computes the altitude in degrees of a given object at the given utc time
+        ra dec in degrees
+        yr mon day hr minu in utc
+        lon lat are in degrees and lon is positive to the East
+        retSun means that you'll get height of the sun above the horizon
+        retDistMoon means that you'll also get the distance to the moon
+    """
+
+    obs = ephem.Observer()
+    obs.lon = lon  # longitude
+    obs.lat = lat  # latitude
+    obs.elevation = elev
+    fb = ephem.FixedBody()
+    fb._ra = np.deg2rad(ra)
+    fb._dec = np.deg2rad(dec)
+    obs.date = ephem.Date('%d/%02d/%d %d:%d:0' % (yr, mon, day, hr, minu))
+    fb.compute(obs)
+    alt = np.rad2deg(1 * fb.alt)
+    az = np.rad2deg(1 * fb.az)
+    if retSun:
+        fbSun = ephem.Sun()
+        fbSun.compute(obs)
+        altSun = np.rad2deg(1 * (fbSun.alt))
+    if retDistMoon:
+        fbMoon = ephem.Moon()
+        fbMoon.compute(obs)
+        altMoon = np.rad2deg(1 * fbMoon.alt)
+        azMoon = np.rad2deg(1 * fbMoon.az)
+        distMoon = sphdist.sphdist(az, alt, azMoon, altMoon)
+    if retSun or retDistMoon:
+        ret = [alt]
+    else:
+        ret = alt
+    if retSun:
+        ret.append(altSun)
+    if retDistMoon:
+        ret.append(distMoon)
+    return ret
