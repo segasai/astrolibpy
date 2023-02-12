237f54d8d91f9e25455eb7057a6617005904e6cb
Sergey E. Koposov <skoposov@ed.ac.uk>
2023-02-12 16:46:48 +0000

    refactor crossmatcher

--- my_utils/crossmatcher.py
+++ my_utils/crossmatcher.py
@@ -2,6 +2,19 @@ import sqlutilpy
 import numpy as np
 
 
+def _failure_cleanup(conn, connSupplied):
+    """ Internal cleanup """
+    try:
+        conn.rollback()
+    except Exception:
+        pass
+    if not connSupplied:
+        try:
+            conn.close()  # do not close if we were given the connection
+        except Exception:
+            pass
+
+
 def doit(tabname,
          ra,
          dec,
@@ -18,7 +31,9 @@ def doit(tabname,
          password=None,
          asDict=False,
          conn=None,
-         preamb=None):
+         preamb=None,
+         intNullVal=-9999,
+         strLength=20):
     """
     Performs the nearest neighbor crossmatch within specified radius
     with the remote table the input is given by ra,dec columns
@@ -64,12 +79,81 @@ def doit(tabname,
     if extra is None:
         extra = 'true'
     your_ra, your_dec = yourradeccols
-    preamb = '' or preamb
-    RES = sqlutilpy.local_join(
-        str.format(
-            """
+    if preamb is None:
+        preamb = ''
+    connSupplied = (conn is not None)
+    mytable = 'mytable'
+    rowid_column = 'xid'
+    arrays = (ra, dec, np.arange(len(ra)))
+    names = (your_ra, your_dec, rowid_column)
+    if not connSupplied:
+        conn = sqlutilpy.getConnection(db=db,
+                                       driver='psycopg2',
+                                       user=user,
+                                       password=password,
+                                       host=host,
+                                       port=(port or 5432))
+    try:
+        if len(arrays[0]) > 1:
+            analyze = True
+        else:
+            analyze = False
+        sqlutilpy.upload(mytable,
+                         arrays,
+                         names,
+                         conn=conn,
+                         noCommit=True,
+                         temp=True,
+                         analyze=analyze)
+    except BaseException:
+        _failure_cleanup(conn, connSupplied)
+        raise
+
+    RES = _run_query(colstring,
+                     tabname,
+                     racol,
+                     deccol,
+                     rad,
+                     mytable,
+                     your_ra,
+                     your_dec,
+                     rowid_column,
+                     preamb=preamb,
+                     conn=conn,
+                     connSupplied=connSupplied,
+                     strLength=strLength,
+                     asDict=asDict,
+                     intNullVal=intNullVal,
+                     extra=extra,
+                     tab_alias=tab_alias)
+
+    conn.rollback()
+    if not connSupplied:
+        conn.close()
+
+    return RES
+
+
+def _run_query(colstring,
+               tabname,
+               racol,
+               deccol,
+               rad,
+               mytable,
+               your_ra,
+               your_dec,
+               rowid_column,
+               extra='true',
+               tab_alias='tt',
+               preamb='',
+               conn=None,
+               connSupplied=False,
+               strLength=20,
+               asDict=None,
+               intNullVal=-9999):
+    query = f"""
             select {colstring} from
-                  ( select * from mytable order by
+                  ( select * from {mytable} order by
             q3c_ang2ipix({your_ra},{your_dec})
                   ) as m
             left join lateral (select * from {tabname} as s where
@@ -77,17 +161,22 @@ def doit(tabname,
             s.{racol}, s.{deccol}, {rad}/3600.) and {extra}
                 order by q3c_dist(m.{your_ra}, m.{your_dec},
                 s.{racol},s.{deccol}) asc limit 1) as {tab_alias}
-            on true order by xid """, **locals()),
-        'mytable', (ra, dec, np.arange(len(ra))), (your_ra, your_dec, 'xid'),
-        preamb=('set enable_seqscan to off;' + 'set enable_mergejoin to off;' +
-                'set enable_hashjoin to off;' + (preamb or '')),
-        host=host,
-        db=db,
-        user=user,
-        port=(port or 5432),
-        password=password,
-        asDict=asDict,
-        conn=conn)
+            on true order by {rowid_column} """
+
+    preamb = ('set enable_seqscan to off;' + 'set enable_mergejoin to off;' +
+              'set enable_hashjoin to off;' + preamb)
+
+    try:
+        RES = sqlutilpy.get(query,
+                            conn=conn,
+                            preamb=preamb,
+                            strLength=strLength,
+                            asDict=asDict,
+                            intNullVal=intNullVal)
+    except BaseException:
+        _failure_cleanup(conn, connSupplied)
+        raise
+
     return RES
 
 
@@ -109,7 +198,7 @@ def doit_by_key(tabname,
                 preamb=None):
     """
     Performs the crossmatch by id
-    with the remote table 
+    with the remote table
 
     Parameters:
     -----------
